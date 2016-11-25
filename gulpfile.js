708bdc2ab6ed9a57bae5d0a69def28b0645c7ac9
jack <418100347@qq.com>
2016-11-25 17:19:12 +0800

    add minjs task
    add minjs task-des

--- gulpfile.js
+++ gulpfile.js
@@ -16,9 +16,14 @@ gulp.task('styles',function(){
 gulp.task('scripts',function(){
 	 gulp.src('./src/coffee/*.coffee')
     .pipe(coffee())
+    .pipe(gulp.dest('./dist/js'))
+});
+gulp.task('scripts_min',function(){
+	 gulp.src('./src/coffee/*.coffee')
+    .pipe(coffee())
     .pipe(uglyfly())
     .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./dist/js'))
+    .pipe(gulp.dest('./dist/min/js'))
 });
 gulp.task('templates',function(){
 	 gulp.src('./views/*.pug')
@@ -27,11 +32,11 @@ gulp.task('templates',function(){
 });
 gulp.task('watch',function(){
   gulp.watch('./src/less/*.less',['styles']);
-  gulp.watch('./src/coffee/*.coffee',['scripts']);
+  gulp.watch('./src/coffee/*.coffee',['scripts','scripts_min']);
   gulp.watch('./views/**/*.pug',['templates']);
   gulp.watch('./gulpfile.js',['watch']); 
 })
 gulp.task('default',function(){
 	//place code for your default task here
-	gulp.start('styles','scripts','templates');
+	gulp.start('styles','scripts','scripts_min','templates');
 });
